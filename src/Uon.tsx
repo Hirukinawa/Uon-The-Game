923119206839425f1b98d45bd710557f4d5746c1
João Pedro <67989586+Hirukinawa@users.noreply.github.com>
2024-05-05 20:47:29 -0300

    v3 - Adicionado Router Dom

--- src/Uon.tsx
+++ src/Uon.tsx
@@ -1,5 +1,37 @@
-function Uon() {
+import { useEffect, useState } from "react";
+import { Player } from "./model/Player";
+import { Game } from "./model/Game";
+import { ICarta } from "./model/Carta";
+import Carta from "./view/Carta";
+import baralho from "./model/Baralho";
 
+export default function Uon() {
+    const [players, setPlayers] = useState<Player[]>([{id: 0, cards: baralho, comprar: () => {}}]);
+    const [actualPlayer, setActualPlayer] = useState(new Player(0, baralho));
+    const [game] = useState(new Game);
+
+    function printCartas(cards: ICarta[]) {
+    return cards.map((carta: ICarta) => {
+        return <Carta key={carta.id} id={carta.id} name={carta.name} color={carta.color} power={carta.power}></Carta>
+        })
+    }
+
+    useEffect(() => {
+        setPlayers(game.gameStart(players, actualPlayer));
+    }, []);
+
+    useEffect(() => {
+        setActualPlayer(players[0]);
+    },[players]);
+
+    return (
+        <section>
+            <h1>Uon</h1>
+            <h2>The Game</h2>
+            <h3>Cartas:</h3>
+            <div className='deck'>
+                {players[0].cards.length > 1 ? printCartas(players[0].cards) : printCartas(baralho)}
+            </div>
+        </section>
+    )
 }
-
-export default Uon;
--- src/view/Carta.tsx
+++ src/view/Carta.tsx
@@ -2,10 +2,26 @@ import { ICarta } from "../model/Carta";
 import "./Carta.css";
 
 export default function Carta(card: ICarta) {
+
+    function contentNameCard(name: string): string {
+        if (name == "block") {
+            return "Ø";
+        } else if (name == "reverse") {
+            return "⮂";
+        } else if (name == "change_color") {
+            return "⭖";
+        } else {
+            return card.name
+        }
+        /* {card.name == "block" ? "Ø" : card.name == "reverse" ? "⮂" : card.name == "change_color" ? "⭖" : card.name} */
+    }
+
     return (
         <div className="cards" style={{backgroundColor: card.color}}>
             <div className="content">
-            <h1>{card.name == "block" ? "Ø" : card.name == "reverse" ? "⮂" : card.name == "change_color" ? "⭖" : card.name}</h1>
+                <div style={{backgroundColor: card.color}}>
+                    <h1 style={{color: "white"}}>{contentNameCard(card.name)}</h1>
+                </div>
             </div>
         </div>
     )
